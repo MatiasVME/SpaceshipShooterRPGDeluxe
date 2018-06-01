4a220194d73d4ff26ef1355cd0eabfebdfd8ec9d
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-06-01 18:05:40 -0400

    Varias escenas y botones

---
+++ Game/Autoloads/Main.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const RES_X = 400
+const RES_Y = 640
+const VERSION = "v0.1.0"
--- Game/MainScreens/Menu.gd
+++ Game/MainScreens/Menu.gd
@@ -1,2 +1,13 @@
 extends Node
 
+func _on_Play_pressed():
+	get_tree().change_scene("res://Game/MainScreens/PlayerMenu.tscn")
+
+func _on_Options_pressed():
+	get_tree().change_scene("res://Game/MainScreens/Options.tscn")
+
+func _on_Credits_pressed():
+	get_tree().change_scene("res://Game/MainScreens/Credits.tscn")
+
+func _on_Shop_pressed():
+	get_tree().change_scene("res://Game/MainScreens/Shop.tscn")
